576f8a60e042cf1f73ac49c33e7f66bea6cd3841
arianna choza <unachoza@gmail.com>
2020-01-23 15:10:18 -0500

    using hash

--- server.js
+++ server.js
@@ -18,94 +18,119 @@ const db = knex({
 console.log(db.select('*').from('users'));
 
 app.use(cors());
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 app.get('/', (req, res) => res.send(db));
 
 app.post('/signin', async (req, res) => {
-  // bcrypt.compare(req.body.password, hash, (err, res) => {
-  //   console.log('in there', res)
-  const { email, password } = req.body;
-  const { users } = db;
-  const thisUSer = await db.select('*').from('users').where({'email': email})
-  
-  try {
-    
-    if (thisUSer.email === users[i].email && thisUSer.password == users[i].password) {
-      return res.json("this is the user", db.users[i])
-    }
-  }
-  catch (error) {
-    console.log(error)
-    res.status(400).json('wrong credentials')
-  // }
-  }
-}),
-  
-  
-app.post('/register', async (req, res) => {
-  const { email, name, password } = req.body;
-  const hash = bcrypt.hashSync(password);
-  const response = await db('users')
-    .returning("*")
-    .insert({
-    name: name,
-    email: email,
-    joined: new Date(),
+  db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        return db.select('*').from('users')
+          .where('email', '=', req.body.email)
+          .then(user => {
+            res.json(user[0])
+          })
+          .catch(err => res.status(400).json('unable to get user'))
+      } else {
+        res.status(400).json('wrong credentials')
+      }
     })
-  .catch(error => res.status(400).json(error)) 
- 
-  return res.json(response)
-}),
-
-app.put('/image', (req, res) => {
-  const { id } = req.body;
-  console.log(req.body);
-  const { users } = db;
-  for (let i = 0; i < users.length; i++) {
-    if (id === users[i].id) {
-      console.log(true);
-      users[i].entries++;
-      return res.json(db.users[i].entries);
-    }
-  }
+    .catch(err => res.status(400).json('wrong credentials'))
 })
 
+  app.post('/register', async (req, res) => {
+    const { email, name, password } = req.body;
+  const hash = bcrypt.hashSync(password);
+ const result = await db.transaction(trx => {
+      trx.insert({
+        hash: hash,
+        email: email
+      })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date()
+          })
+          .then(user => {
+            res.json(user[0]);
+          })
+      })
+          
+      .then(trx.commit)
+      // .catch(trx.rollback)
+ })
+    // console.log(result)
+    console.log('it strts here0000000000' , res.json(result))
+     return res.json(result)
+    //  console.log(reg)
+    // .catch(err => res.status(400).json('unable to register'))
+})
 
-  // return res.json(db.users[db.users.length - 1]);
-  // db
-  //   .transaction(trx => {
-  //     trx
-  //       .insert({
-  //         hash: hash,
-  //         email: email,
-  //       })
-  //       .into('login')
-  //       .returning('email')
-  //       .then(email => {
-  //         return trx('users')
-  //           .returning('*')
-  //           .insert({
-  //             email: email[0],
-  //             name: name,
-  //             joined: new Date(),
-  //           })
-  //           .then(user => {
-  //             res.json(user[0]);
-  //           });
-  //       })
-  //       .then(trx.commit)
-  //       .catch(trx.rollback);
-  //   })
-  // .catch(err => res.status(400).json('unable to register'));
+  //   const { email, name, password } = req.body;
+  //   const hash = bcrypt.hashSync(password);
+  //   const response = await db('users')
+  //     .returning('*')
+  //     .insert({
+  //       name: name,
+  //       email: email,
+  //       joined: new Date(),
+  //     })
+  //     .catch(error => res.status(400).json(error));
+
+  //   return res.json(response);
+  // }),
+  // app.put('/image', async (req, res) => {
+  //   const { id } = req.body;
+  //   const newEntriesCount = await db('users')
+  //     .where('id', '=', id)
+  //     .increment('entries', 1)
+  //     .returning('entries');
+
+  //   return res.json(newEntriesCount[0]);
+  // });
+
+// return res.json(db.users[db.users.length - 1]);
+// db
+//   .transaction(trx => {
+//     trx
+//       .insert({
+//         hash: hash,
+//         email: email,
+//       })
+//       .into('login')
+//       .returning('email')
+//       .then(email => {
+//         return trx('users')
+//           .returning('*')
+//           .insert({
+//             email: email[0],
+//             name: name,
+//             joined: new Date(),
+//           })
+//           .then(user => {
+//             res.json(user[0]);
+//           });
+//       })
+//       .then(trx.commit)
+//       .catch(trx.rollback);
+//   })
+// .catch(err => res.status(400).json('unable to register'));
 // });
 
 app.get('/profile/:id', async (req, res) => {
   const { id } = req.params;
-  const thisUSer = await db.select('*').from('users').where({ "id": id })
-  console.log(thisUSer)
-return thisUSer.length? res.json(thisUSer) :res.status(400).json('user not found');
-  
-    
+  const thisUSer = await db
+    .select('*')
+    .from('users')
+    .where({ id: id });
+  console.log(thisUSer);
+  return thisUSer.length ? res.json(thisUSer) : res.status(400).json('user not found');
 }),
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+  app.listen(3000, () => console.log('Example app listening on port 3000!'));
